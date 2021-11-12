118093d0cec249755d88d1a7fc05f12eb541095d
Ditaaprilliani <75934255+Ditaaprilliani@users.noreply.github.com>
2021-11-12 16:54:53 +0700

    Add files via upload

---
+++ UTS/script.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $('#menu').click(function(){
+      $(this).toggleClass('fa-times');
+      $('.navbar').toggleClass('nav-toggle');
+    });
+
+    $(window).on('load scroll',function(){
+      $('#menu').removeClass('fa-times');
+      $('.navbar').removeClass('nav-toggle');
+
+      if($(window).scrollTop() > 0){
+        $('.scroll-top').show();
+      }else{
+        $('.scroll-top').hide();
+      }
+
+      $('section').each(function(){
+        let top = $(window).scrollTop();
+        let offset = $(this).offset().top - 200;
+        let id = $(this).attr('id');
+        let height = $(this).height();
+        if(top > offset && top < offset + height){
+          $('.navbar a').removeClass('active');
+          $('.navbar a').find(`[href="#{id}]`).addClass('active');
+        }
+      });
+    });
+});
